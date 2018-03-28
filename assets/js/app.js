1fc58b497ee1e87cd12993b8081e2525800c9b24
kader amrichat <kader.amrichat@gmail.com>
2018-03-28 18:15:43 +0200

    Finalisation css et fonction js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,14 @@
 const app = (function(){
   "use strict";
 
-let choice01 = "";
-let choice02 = "";
+let choice01 = "";//choix player 01
+let choice02 = "";//choix player 02
+let scoreJ1 = 0;
+let scoreJ2 = 0;
 
 
 //Plutôt que de faire des tests de conditions on va regarder si la valeur du player01 possède les valeur du player 02
 //hasOwnProperty
-
 let list = {
     "pierre" : {
               "ciseaux" : "émousse",
@@ -38,27 +39,32 @@ const _ = function(id){
   return document.getElementById(id);
 };
 
-//Initialisaiton de l'app quand tout est chargé
+//Fonction de selection par selecteur
+const _All = function(elems){
+  return document.querySelectorAll(elems);
+};
+
 const init = function(){
   //Listener sur btn joueur 01
-   _("pierre-p1").onclick  = getValueP1;
-   _("feuille-p1").onclick = getValueP1;
-   _("ciseaux-p1").onclick = getValueP1;
-   _("lezard-p1").onclick  = getValueP1;
-   _("spock-p1").onclick   = getValueP1;
+   _All(".card-p1").forEach((elem)=> {
+     elem.onclick = getValueP1;
+   });
 
   //Listener sur btn joueur 02
-   _("pierre-p2").onclick  = getValueP2;
-   _("feuille-p2").onclick = getValueP2;
-   _("ciseaux-p2").onclick = getValueP2;
-   _("lezard-p2").onclick  = getValueP2;
-   _("spock-p2").onclick   = getValueP2;
+   _All(".card-p2").forEach((elem)=> {
+     elem.onclick = getValueP2;
+   });
 
    _("get-result").onclick  = () => {
      if((choice01 !== "") && (choice02 !== "")){
        compare(choice01, choice02);
+      //On réinitialise pour recommencer
+       choice01 = "";
+       choice02 = "";
      }
    };
+
+   _("reset").onclick = resetFn;
 };
 
 //Récupérer la valeur du btn joueur 01 et la stocker dans une variable
@@ -68,7 +74,7 @@ const getValueP1 = () => {
     choice01 = target.value;
     display(choice01, "output-player01",1);
   }else {
-    console.log("Player01 a déjà choisi");
+    _("result").innerHTML = "Le joueur 01 a déjà joué";
   }
 };
 
@@ -77,30 +83,43 @@ const getValueP2 = () => {
   let target = event.target || event.srcElement;
   if(choice02 === ""){
     choice02 = target.value;
-    display(choice02, "output-player02",1);
+    display(choice02, "output-player02",2);
   }else {
-    console.log("Player02 a déjà choisi");
+    _("result").innerHTML = "Le joueur 02 a déjà joué";
   }
 };
 
 
 //Afficher le resultat dans une div
 const display = (choice, output, num) => {
-    _(output).innerHTML = `le choix du joueur ${num} est ${choice}`;
+    _(output).innerHTML = `<span class="msg-choice">${choice}</span>`;
 };
 
 //fonction de comparaison des résultats
 const compare = function(choice01, choice02){
-  console.log(list);
-  if(list[choice01].hasOwnProperty(choice02)){
-    _("result").innerHTML = "Joueur 01 gagne !!! <br> " + choice01 + " " + list[choice01][choice02] + " " + choice02 ;
-    console.log("Player01 Win !!!!");
+  if(choice01 === choice02){
+    _("result").innerHTML = "Egalité";
+  }
+  else if(list[choice01].hasOwnProperty(choice02)){
+    _("result").innerHTML = ` >> Le joueur 01 gagne !!! << <span class="explication">  (${choice01} bat  ${choice02})</span>` ;
+    scoreJ1++;
+    _("scoreJ1").innerHTML = scoreJ1;
   } else {
-    _("result").innerHTML = "Joueur 02 gagne";
-    console.log("Player02 Win !!!!");
+    _("result").innerHTML =   `>> Le joueur 02 gagne << <span class="explication">${choice02} bat ${choice01}</span>  `;
+    scoreJ2++;
+    _("scoreJ2").innerHTML = scoreJ2;
   }
 };
 
+
+//Fonction de resetFn (les scores & les affichages)
+const resetFn = () => {
+  scoreJ1 = 0;
+  _("scoreJ1").innerHTML  = scoreJ1;
+  scoreJ2 = 0;
+  _("scoreJ2").innerHTML  = scoreJ1;
+  _("result").innerHTML   = "";
+};
 
 window.addEventListener("DOMContentLoaded",init); //On attend que le DOM se charge avant de lancer init
 
